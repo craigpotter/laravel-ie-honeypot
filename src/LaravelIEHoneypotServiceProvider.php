1860105bf8eb7f956e3d93988305957da80b6431
Craig Potter <me@craigpotter.co.uk>
2021-04-23 18:52:46 +0100

    remove comment

--- src/LaravelIEHoneypotServiceProvider.php
+++ src/LaravelIEHoneypotServiceProvider.php
@@ -10,11 +10,6 @@ class LaravelIEHoneypotServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-ie-honeypot')
             ->hasConfigFile()
